4d46bbbc1baffd596b5612bcebf142f6b99b4c71
Virjanand Panday <virjanand@virjanand.com>
2022-07-13 09:41:10 +0200

    extract function for fizzRule

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,19 +1,22 @@
 package fizzbuzz;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
     public List<String> getNumbers(int from, int until) {
-        return IntStream.rangeClosed(from, until).mapToObj(this::toFizzBuzz).collect(Collectors.toList());
+        return IntStream.rangeClosed(from, until).mapToObj(number -> toFizzBuzz(number, number1 -> {
+            if (number1 % 3 == 0) {
+                return "Fizz";
+            }
+            return "";
+        })).collect(Collectors.toList());
     }
 
-    private String toFizzBuzz(int number) {
-            String result = "";
-        if (number % 3 == 0) {
-            result = "Fizz";
-        }
+    private String toFizzBuzz(int number, Function<Integer, String> fizzRule) {
+        String result = fizzRule.apply(number);
         if (number % 5 == 0) {
             result += "Buzz";
         }
